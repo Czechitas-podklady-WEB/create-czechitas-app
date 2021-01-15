eee4e09d47bbad959758024c72feb224a2c0f9b7
Martin Podloucký <podlouckymartin@gmail.com>
2021-01-15 15:40:40 +0100

    Create React app by default. Create vanilla app by using --no-react.

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 A script to create the structure of a web application based on Webpack and vanilla JavaScript.
 
-To create and run a fresh new app
+To create and run a fresh new React app 
 
 ```bash
 npx create-czechitas-app my-app
 cd my-app
 npm start
+```
+
+If you want to have a vanilla JavaScript application without React use
+
+```bash
+npx create-czechitas-app my-app --no-react
 ```
--- index.js
+++ index.js
@@ -5,15 +5,33 @@
 const fs = require('fs-extra');
 const path = require('path');
 const spawnSync = require('cross-spawn').sync;
+const yargs = require('yargs/yargs');
 
-const projectName = process.argv[2];
-const root = path.resolve(projectName);
+const argv = yargs(process.argv.slice(2))
+  .command({
+    command: '$0 <app_name>', 
+    desc: 'Create a starter web application', 
+    builder: (yargs) => yargs
+      .positional('app_name', {
+        describe: 'The name of your application',
+        type: 'string'
+      })
+      .option('react', {
+        desc: 'Create a React appliaction',
+        type: 'boolean',
+        default: true,
+      })
+    }).help().argv;
 
+const root = path.resolve(argv.app_name);
 fs.mkdirSync(root);
-fs.copySync(path.resolve(__dirname, 'project-tree'), root);
+
+const projectTree = argv.react ? 'react-tree' : 'vanilla-tree';
+fs.copySync(path.resolve(__dirname, projectTree), root);
+fs.copySync(path.resolve(__dirname, 'common-files'), root);
 
 const packageJson = fs.readJsonSync(path.resolve(root, 'package.json'));
-packageJson.name = projectName;
+packageJson.name = argv.app_name;
 fs.writeJsonSync(path.resolve(root, 'package.json'), packageJson, {
   spaces: 2,
 });

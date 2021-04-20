f747392a2bf9f144469810d8a77388c691c4f044
Martin Podloucký <podlouckymartin@gmail.com>
2021-04-20 17:59:35 +0200

    Transition to using whipapp-core.

--- index.js
+++ index.js
@@ -2,72 +2,17 @@
 
 'use strict';
 
-const fs = require('fs');
 const path = require('path');
-const walkSync = require('walkdir').sync;
+const chalk = require('chalk');
 const spawnSync = require('cross-spawn').sync;
 const yargs = require('yargs/yargs');
-const chalk = require('chalk');
+const { fetchKitPlan, initAppFolder, generateApp } = require('whipapp-core');
 
-// Copies the content of a given directory to the destination directory
-// Renames file '_.gitignore' to '.gitignore'
-function copyDir(dir, dest) {
-  return walkSync(dir, (srcPath, stat) => {
-    const srcFileName = path.basename(srcPath);
-    const destFileName =
-      srcFileName === '_.gitignore' ? '.gitignore' : srcFileName;
-
-    const srcFileParent = path.dirname(srcPath);
-
-    const destPath = path.resolve(
-      dest,
-      path.relative(dir, srcFileParent),
-      destFileName,
-    );
-
-    if (stat.isDirectory()) {
-      fs.mkdirSync(destPath);
-      return;
-    }
-
-    fs.copyFileSync(srcPath, destPath);
-  });
-}
-
-function createApp(appName, useReact) {
-  if (useReact) {
-    console.log('Creating a React application.');
-  } else {
-    console.log('Creating a vanilla JS application.');
-  }
-
-  const root = path.resolve(appName);
-  fs.mkdirSync(root);
-
-  const treeName = useReact ? 'react-tree' : 'vanilla-tree';
-
-  copyDir(path.resolve(__dirname, 'project-trees', 'common'), root);
-  copyDir(path.resolve(__dirname, 'project-trees', treeName), root);
-
-  const packageJson = JSON.parse(
-    fs.readFileSync(path.resolve(root, 'package.json')),
-  );
-  packageJson.name = appName;
-  fs.writeFileSync(
-    path.resolve(root, 'package.json'),
-    JSON.stringify(packageJson, { spaces: 2 }),
-  );
-
-  console.log('Installing NPM dependencies:');
-
-  spawnSync('npm', ['install'], { cwd: root, stdio: 'inherit' });
-
-  console.log(chalk.green(`Project '${appName}' created successfully.`));
-}
+const REPO_URL = 'https://api.github.com/repos/podlomar/czechitas-starter-kits';
 
 const argv = yargs(process.argv.slice(2))
   .command({
-    command: '$0 <app_name>',
+    command: '$0 <app_name> <kit_name>',
     desc: 'Create a starter web application',
     builder: (yargs) =>
       yargs
@@ -75,16 +20,27 @@ const argv = yargs(process.argv.slice(2))
           describe: 'The name of your application',
           type: 'string',
         })
-        .option('react', {
-          desc: 'Create a React application',
-          type: 'boolean',
-          default: true,
+        .positional('kit_name', {
+          desc: 'Starter kit template',
+          type: 'string',
+          default: 'react',
         }),
-  })
-  .help().argv;
+    }).help().argv;
 
-try {
-  createApp(argv.app_name, argv.react);
-} catch (error) {
-  console.error(chalk.redBright(`ERROR: ${error.message}`));
-}
+(async () => {
+  try {
+    console.log(argv.app_name);
+    console.log(argv.kit_name);
+    const rootDir = path.resolve('.');
+    const kitPlan = await fetchKitPlan(argv.kit_name, REPO_URL);
+    const { appRoot } = initAppFolder(rootDir, argv.app_name);
+    await generateApp(argv.app_name, kitPlan);
+  
+    console.log('Installing NPM dependencies:');
+    spawnSync('npm', ['install'], { cwd: appRoot, stdio: 'inherit' });
+
+    console.log(chalk.green(`Project '${argv.app_name}' created successfully.`));
+  } catch (error) {
+    console.error(chalk.redBright(`ERROR: ${error.message}`));
+  }
+})();

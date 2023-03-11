c35917d65680a2d04d975177895147380cbd7b61
Martin Podloucký <podlouckymartin@gmail.com>
2023-03-11 16:50:11 +0100

    Discontinue the package

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
 # create-czechitas-app
 
-**!! This package is no longer maintained. Use [create-kodim-app](https://www.npmjs.com/package/create-kodim-app) instead. !!**
-
-A script to create the structure of a JavaScript web application based on Webpack.
-
-To create and run a fresh new React app 
-
-```bash
-npx create-czechitas-app my-app
-cd my-app
-npm start
-```
-
-If you want to have a vanilla JavaScript application without React use
-
-```bash
-npx create-czechitas-app my-app vanilla
-```
+**!! This package is discontinued and will no longer generate any projects. Use [create-kodim-app](https://www.npmjs.com/package/create-kodim-app) instead. !!**
--- index.js
+++ index.js
@@ -1,59 +1,14 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
+
 'use strict';
 
-const path = require('path');
-const chalk = require('chalk');
-const spawnSync = require('cross-spawn').sync;
-const yargs = require('yargs/yargs');
-const { fetchKitPlan, initAppFolder, generateApp } = require('whipapp-core');
+console.info(chalk.red('DISCONTINUATION NOTICE:'));
 
-const DEFAULT_SERVER_URL = 'https://podlomar.github.io/czechitas-starter-kits';
+console.info(`
+This package is discontinued and will no longer generate any projects!
+Use the successor package create-kodim-app like this:
 
-const argv = yargs(process.argv.slice(2))
-  .command({
-    command: '$0 <app_name> [kit_name]',
-    desc: 'Create a starter web application',
-    builder: (yargs) => yargs
-      .positional('app_name', {
-        describe: 'The name of your application',
-        type: 'string',
-      })
-      .positional('kit_name', {
-        desc: 'Starter kit template',
-        type: 'string',
-        default: 'react',
-      })
-      .option('s', {
-        alias: 'server',
-        default: DEFAULT_SERVER_URL,
-        describe: 'URL from where to fetch the kit files',
-        type: 'string'
-    })
-  }).help().argv;
-
-(async () => {
-  try {
-    const rootDir = path.resolve('.');
-    const kitPlan = await fetchKitPlan(argv.kit_name, argv.server);
-    if (kitPlan.status === 'error') {
-      console.error(chalk.redBright('ERROR:', kitPlan.message));
-      return;
-    }
-
-    const result = initAppFolder(rootDir, argv.app_name);
-    if (result.status === 'error') {
-      console.error(chalk.redBright('ERROR:', result.message));
-      return;
-    }
-
-    await generateApp(result.appRoot, kitPlan);
-  
-    console.info('Installing NPM dependencies:');
-    spawnSync('npm', ['install'], { cwd: result.appRoot, stdio: 'inherit' });
-
-    console.info(chalk.green(`Project '${argv.app_name}' created successfully.`));
-  } catch (error) {
-    console.error(chalk.redBright(`ERROR: ${error.message}`));
-  }
-})();
+npm init kodim-app my-app
+`);
